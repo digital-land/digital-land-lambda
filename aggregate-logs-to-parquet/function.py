caa57eec45ccda28d6d96190f9c7e052b16cf1b6
George Goodall <ggoodall@scottlogic.com>
2024-01-24 10:53:17 +0000

    remove duckdb for test

--- aggregate-logs-to-parquet/function.py
+++ aggregate-logs-to-parquet/function.py
@@ -1,4 +1,4 @@
-import duckdb
+# import duckdb
 import boto3
 
 logBucket = 'development-data-val-be-cdn-logs'
@@ -8,14 +8,14 @@ filename = 'combined.log'
 def lambda_handler(event, context):
     copyFileTest()
 
-def saveLogsToParquet():
-    con = duckdb.connect()
-    con.install_extension('https')
-    con.load_extension('https')
-    con.execute("SET s3_region='eu-west-2';")
-    con.execute("SET s3_access_key_id='AKIA55A7WSGA7H2V4GHR';")
-    con.execute("SET s3_secret_access_key='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';")
-    con.execute(f"COPY (SELECT * FROM read_json('s3://bucketName/*.json') to 's3://bucketName/parquet/' (format 'parquet');")
+# def saveLogsToParquet():
+#     con = duckdb.connect()
+#     con.install_extension('https')
+#     con.load_extension('https')
+#     con.execute("SET s3_region='eu-west-2';")
+#     con.execute("SET s3_access_key_id='AKIA55A7WSGA7H2V4GHR';")
+#     con.execute("SET s3_secret_access_key='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';")
+#     con.execute(f"COPY (SELECT * FROM read_json('s3://bucketName/*.json') to 's3://bucketName/parquet/' (format 'parquet');")
 
 
 # this function should copy the combined.log file from the CDN logs bucket to the reporting bucket
