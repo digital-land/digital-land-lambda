114bac06ac1e62ee580652df3e4569e8c267392a
George Goodall <ggoodall@scottlogic.com>
2024-02-22 10:00:45 +0000

    change to only get last 12 hours of logs

--- aggregate-logs-to-parquet/function.py
+++ aggregate-logs-to-parquet/function.py
@@ -14,9 +14,10 @@ parquet_file_path = f"s3://{reportBucket}"
 def lambda_handler(event, context):
     last_midnight = datetime.datetime.combine(datetime.datetime.today(), datetime.time.min)
     one_day = datetime.timedelta(days=1)
+    twelve_hours = datetime.timedelta(hours=12)
     schemas = {
         'PageView': pageViewSchema
     }
 
-    logCombiner = LogCombiner('/application/development-data-val-fe', schemas, last_midnight - one_day, last_midnight)
+    logCombiner = LogCombiner('/application/development-data-val-fe', schemas, last_midnight - twelve_hours, last_midnight)
     logCombiner.combineLogs(parquet_file_path)
