cff37529ce4981022367b126c0d4e4017dfa6d54
George Goodall <ggoodall@scottlogic.com>
2024-02-26 14:31:51 +0000

    added a call to get the last 30 logs on first run

--- aggregate-logs-to-parquet/function.py
+++ aggregate-logs-to-parquet/function.py
@@ -22,5 +22,29 @@ def run():
         'PageView': pageViewSchema
     }
 
-    logCombiner = LogCombiner('/application/development-data-val-fe', schemas, last_midnight - one_day, last_midnight)
+    logCombiner = LogCombiner('/application/production-data-val-fe', schemas, last_midnight - one_day, last_midnight)
     logCombiner.combineLogs(parquet_file_path)
+
+def run_custom(start, end, schemas):
+    logCombiner = LogCombiner('/application/production-data-val-fe', schemas, start, end)
+    logCombiner.combineLogs(parquet_file_path)
+
+
+
+from datetime import datetime, time, timedelta
+
+# Get the current time
+now = datetime.now()
+
+# Define the cut-off time
+cut_off = datetime(2024, 2, 26, 15, 0)  # 3 PM
+
+# Check if the current time is earlier than the cut-off time
+if now < cut_off:
+    last_midnight = datetime.combine(datetime.today(), time.min)
+    one_day = timedelta(days=1)
+    for i in range(30):
+        start = last_midnight - one_day * i
+        end = start + one_day
+        run_custom(start, end, schemas)
+        print(f"Finished day {i+1} of 30")
